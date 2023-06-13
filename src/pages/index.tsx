318412474f47dfb41459f3c350619ce6a17e570b
David Bennet <you@example.com>
2023-06-13 00:52:46 -0300

    header, home and imagens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,31 @@
-import { styled } from "../styles"
+import Image from "next/image";
+import { Homecontainer, Product } from "../styles/pages/home";
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-  
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  }
-})
+import camiseta1 from "../assets/camisetas/1.png"
+import camiseta2 from "../assets/camisetas/2.png"
+import camiseta3 from "../assets/camisetas/3.png"
+import camiseta4 from "../assets/camisetas/4.png"
 
 export default function Home() {
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <Homecontainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={420} height={380}/>
+
+        <footer>
+          <strong>camiseta A</strong>
+          <span>R$ 50,00</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} alt="" width={420} height={380}/>
+
+        <footer>
+          <strong>camiseta B</strong>
+          <span>R$ 55,00</span>
+        </footer>
+      </Product>
+    </Homecontainer>
   )
 }
